acd1f3ca22d598ce8430e35ab8744bbb756f43bd
Wiktor Sierpiński <vaighir@gmail.com>
2020-07-15 19:49:45 +0200

    Add score to ui controller

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
     public int lives;
+    public int score;
+    public Text scoreText;
     public GameObject[] hearts;
     public Sprite heart;
 
@@ -19,6 +22,9 @@ public class UIController : MonoBehaviour
         GameObject octo = GameObject.FindWithTag("Player");
         OctoController octoController = octo.GetComponent<OctoController>();
         lives = octoController.lives;
+
+        // TODO get score from the appropriate controller
+        score = 0;
     }
 
     // Update is called once per frame
@@ -34,5 +40,7 @@ public class UIController : MonoBehaviour
                 hearts[i].SetActive(false);
             }
         }
+
+        scoreText.text = "SCORE: " + score;
     }
 }
