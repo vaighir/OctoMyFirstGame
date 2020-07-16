41e7a5472ffc735487a61d77ab6160074f449879
Wiktor Sierpiński <vaighir@gmail.com>
2020-07-16 17:56:46 +0200

    Create fish prefab with random sprite

---
+++ Assets/Scripts/FishController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishController : MonoBehaviour
+{
+    public Sprite[] sprites;
+    private Sprite sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeVariables();   
+    }
+
+    private void InitializeVariables()
+    {
+        SpriteRenderer s = GetComponent<SpriteRenderer>();
+        GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, 8)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
