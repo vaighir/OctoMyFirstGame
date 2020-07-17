4df83208d9aab4bd971513700ca1d7c2e256b8f0
Wiktor Sierpiński <vaighir@gmail.com>
2020-07-17 17:28:24 +0200

    Create shark prefab

--- Assets/Scripts/SharkController.cs
+++ Assets/Scripts/SharkController.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class SharkController : MonoBehaviour
 {
+    public Sprite[] sprites;
+    public OctoController octoController;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitializeVariables();
+    }
+
+    private void InitializeVariables()
+    {
+        SpriteRenderer s = GetComponent<SpriteRenderer>();
+        GetComponent<SpriteRenderer>().sprite = sprites[0];
+
+        GameObject octo = GameObject.FindWithTag("Player");
+        octoController = octo.GetComponent<OctoController>();
     }
 
     // Update is called once per frame
